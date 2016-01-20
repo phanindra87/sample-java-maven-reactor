adb44d25e13d223b621e5087134a9e3b5b356388
Phanindra Telukunta <phanindra_t@outlook.com>
2016-01-20 15:26:49 +0530

    Including this line for testing automated build in jenkins
    Including this line for testing automated build in jenkins

--- client/src/main/java/com/shippable/client/App.java
+++ client/src/main/java/com/shippable/client/App.java
@@ -1,7 +1,7 @@
 package com.shippable.client;
 
 import com.shippable.common.MeaningOfEverythingService;
-
+//Including this line for testing automated build in jenkins
 public class App {
   private MeaningOfEverythingService service;
 
